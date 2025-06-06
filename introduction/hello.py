b85e8514d319ed02d9f2bac231412ff4fc835d01
JUNEBusiness <justiceomonigho@gmail.com>
2025-06-06 21:19:17 +0100

    Modified the 'hello.py' file and wrote some examples of data types. I also created a jupyter notebook file to b used for subsequent classes.

--- introduction/hello.py
+++ introduction/hello.py
@@ -1,1 +1,20 @@
 print("Hello World")
+
+'hello world'
+
+# we use single and doublr quotes to denote strings. When we use single quotes to open the 
+# string, it must be closed with a corresrponding quote
+'Kolawole and Udeme came into the google classroom before I did. I think Edozie joined later, on the other hand, They were all "ready, willing and able". So the class commenced'
+#  interger are real numbers both negative and positive 123457788987
+# Floats are decimals, both negative and positive 3.142, 565.35353
+# Booleans True, False
+
+"""Variable names are written in snake case in Python programming language e.g address_for_shops = “No 9 …”"""
+name = "Justice"
+height = "6 ft 2'"
+complexion = "Dark"
+age = 55
+happy = True
+print(name)
+print(12345)
+print("Zummit")
