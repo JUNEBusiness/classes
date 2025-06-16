982efc497c95704d98483428d4911eec906bd41a
JUNEBusiness <justiceomonigho@gmail.com>
2025-06-16 19:39:24 +0100

    Added 'loops.py' file with examples.

---
+++ introduction/loops.py
@@ -0,0 +1,47 @@
+fruits = ["Mango", "Pineapple", "Jute", "Cherry"]
+fruits[0] = "Squid"
+...
+number = int(input("How many times do you want me to Meow? "))
+# LOOPS: They help in repetitive tasks
+# for loops
+# name = "Orji"
+# for i in range(2,4):
+#     fruits[i] = "Squid"
+#     print(fruits)
+
+# length_of_list = len(fruits)
+# print(length_of_list)
+
+# for i in range(4):
+#     fruits[i] = "Squid"
+   
+# print(fruits)
+
+
+# for i in range(4):
+#     print(i)
+
+# print()
+# for i in [0, 1, 2, 3]:
+#     print(i)
+
+# print()
+# for i in ["Mango", "Pineapple", "Jute", "Cherry"]:
+#     print(i)
+
+
+# print()
+# for i in fruits:
+#     print(i)
+
+
+def cat(n):
+    for i in range(n):
+        print("meow!!")
+
+
+def dog(n):
+    print(n)    
+
+
+cat(number)
